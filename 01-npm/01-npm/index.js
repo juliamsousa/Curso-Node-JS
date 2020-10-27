a0893c57163585e7858a816eb0b9b112045eeb38
Júlia Eduarda Miranda de Sousa <39354498+juliamsousa@users.noreply.github.com>
2020-10-27 13:59:04 -0300

    Update index.js

--- 01-npm/01-npm/index.js
+++ 01-npm/01-npm/index.js
@@ -1,3 +1,7 @@
+// codigo de exemplo para criacao do primeiro projeto em node
+// para criar um projeto node utilizar npm init para criar nosso arquivo package.json
+// npm eh o gerenciador de pacotes do node
+// node nao eh uma  lignuagem, eh um ambiente de execucao de codigo javascript
 console.log('Hello node!')
 const a = 1
 const b = 2
