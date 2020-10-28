1e6f1a0b491bf7a906f962b5e89d2bb29506d2cc
Júlia Eduarda Miranda de Sousa <39354498+juliamsousa@users.noreply.github.com>
2020-10-28 08:52:09 -0300

    Update index.js

--- 03-events/03-events/index.js
+++ 03-events/03-events/index.js
@@ -1,33 +1,49 @@
+// codigo de exemplo sobre tratamento e disparo de eventos
+// importacao de 'events'
 const EventEmitter = require('events')
+
+// classe de um emissor que estende a classe 'events'
 class MeuEmissor extends EventEmitter {
 
 }
+
+// cria um novo emissor
 const meuEmissor = new MeuEmissor()
+// nome do evento disparado
 const nomeEvento = 'usuario:click'
+
+// acao que ocorre ao disparo de um evento de click
 meuEmissor.on(nomeEvento, function (click) {
     console.log('um usuario clicou', click)
 })
 
+// evento na barra de rolagem
+meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+meuEmissor.emit(nomeEvento, 'no ok')
 
-// meuEmissor.emit(nomeEvento, 'na barra de rolagem')
-// meuEmissor.emit(nomeEvento, 'no ok')
+let count = 0
 
-// let count = 0
-// setInterval(function () {
-//     meuEmissor.emit(nomeEvento, 'no ok' + (count++))
+// simula o disparo do evento a cada 1s com a funcao set interval
+// a medida que o evento eh disparado o contador eh incrementado
+setInterval(function () {
+    meuEmissor.emit(nomeEvento, 'no ok' + (count++))
+}, 1000)
 
-// }, 1000)
-
+// cria uma variavel de entrada que recebe dados digitados no terminal
 const stdin = process.openStdin()
 
 function main() {
+    // como foi criada com uma promise, apos ser resolvida, o listenner para de ouvir
     return new Promise(function (resolve, reject) {
+        // fica ouvindo, caso o evento seja disparado faz uma acao
         stdin.addListener('data', function (value) {
             // console.log(`Voce digitou: ${value.toString().trim()}`)
             return resolve(value)
         })
     })
 }
+
+// imprime o resultado retornado da funcao main
 main().then(function (resultado) {
     console.log('resultado', resultado.toString())
 })
