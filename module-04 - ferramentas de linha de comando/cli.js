a61549f6f987c13497448ef955453cea5e795631
Júlia Eduarda Miranda de Sousa <juliaeduardasousa@gmail.com>
2020-10-29 11:36:27 -0300

    Comentários da seção module-04/cli.js

--- module-04 - ferramentas de linha de comando/cli.js
+++ module-04 - ferramentas de linha de comando/cli.js
@@ -1,5 +1,11 @@
+// CLI - Command Line Interface
+// o commander permite que criemos comandos para a linha de comandos
+
+// importacao do commander
 const commander = require('commander');
+// importacao da classe Heroi para que manipulemos os dados advindos da linha de comando
 const Heroi = require('./heroi');
+// importacao do banco de dados para que manipulemos os dados do banco
 const Database = require('./database');
 
 (async () => {
@@ -7,6 +13,7 @@ const Database = require('./database');
    * node cli.js --help
    */
   commander
+    // informacao da versao
     .version('v1')
     .option('-n, --nome [value]', 'adicionar nome')
     .option('-p, --poder [value]', 'adicionar poder')
@@ -17,15 +24,23 @@ const Database = require('./database');
     .option('-d, --remover [value]', 'remover heroi pelo id')
     .parse(process.argv);
 
+  // cria um novo heroi com os dados vindos do commander
+  // como a classe apresenta apenas alguns atributos, ignora os atributos restantes vindos do objeto commander
   const heroi = new Heroi(commander);
+
   try {
     /**
      * node cli.js --cadastrar params...
      * node cli.js -c -n Hulk -p Forca
      */
     if (commander.cadastrar) {
+      // cadastra o heroi advindo da linha de comando
       await Database.cadastrar(heroi);
+
+      // mensagem de sucesso no cadastro
       console.log('item cadastrado com sucesso!');
+      
+      // finaliza a funcao
       return;
     }
 
@@ -35,9 +50,16 @@ const Database = require('./database');
      * node cli.js -r 1
      */
     if (commander.listar) {
+      // recebe um id do commander
       const id = commander.listar;
+      
+      // busca o heroi no banco pelo seu id
       const result = await Database.listar(id);
+
+      // imprime o resultado no console
       console.log(result);
+      
+      // finaliza a funcao
       return;
     }
 
@@ -47,10 +69,17 @@ const Database = require('./database');
      * node cli.js -u 1 -n thor -p trovao
      */
     if (commander.atualizar) {
+      // armazena a id recebida do commander
       const id = commander.atualizar;
       console.log('id', id);
+
+      // atualiza o heroi no banco de dados pelo id recebido
       await Database.atualizar(id, heroi);
+
+      // mensagem de sucesso
       console.log('item atualizado com sucesso!');
+      
+      // finaloza a funcao
       return;
     }
     /**
@@ -58,13 +87,23 @@ const Database = require('./database');
      * node cli.js -d 1
      */
     if (commander.remover) {
+      // armazena a id recebida do commander
       const id = commander.remover;
+
+      // remove o item do banco de dados pelo id
       await Database.remover(id);
+
+      // mensagem de sucesso
       console.log('item removido com sucesso!');
+      
+      // finaliza a funcao
       return;
     }
   } catch (error) {
+    // tratamento de erro
     console.error('DEU RUIM', error);
+
+    // finaliza a funcao
     return;
   }
 })();
